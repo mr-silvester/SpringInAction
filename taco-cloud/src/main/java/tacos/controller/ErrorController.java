996f1abd6115b1cfb73b6451f224a631b00894c6
DESKTOP-KK5SP41\新年前夜 <mascheny@naver.com>
2023-05-25 11:17:38 +0900

    주석 추가

--- taco-cloud/src/main/java/tacos/controller/ErrorController.java
+++ taco-cloud/src/main/java/tacos/controller/ErrorController.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
     @RequestMapping
     public String errorPage(HttpServletResponse http) {
-        System.out.println("http status : " + http.getStatus());
+        //http 응답 코드 추출
         int statusCode = http.getStatus();
+        // 403 에러 시 error 403.html 호출
         if ( statusCode == 403 )
             return "error403";
+        // 404 에러 시 error 404.html 호출
         if ( statusCode == 404 )
             return "error404";
         return "error";
