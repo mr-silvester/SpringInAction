56db659f557b6009379341f0d1ea37b596abc02d
silvester <mascheny@naver.com>
2023-07-03 14:00:24 +0900

    add test

--- taco-cloud/src/test/java/tacos/controller/HomeControllerTests.java
+++ taco-cloud/src/test/java/tacos/controller/HomeControllerTests.java
@@ -1,34 +1,21 @@
 package tacos.controller;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import tacos.security.CustomPermissionEvaluator;
 
-import java.util.HashSet;
-
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HomeControllerTests {
     @Autowired
     private MockMvc mockMvc ;
-
-    @InjectMocks
-    private CustomPermissionEvaluator customPermissionEvaluator;
-
-    @Mock
-    private Authentication authentication;
 
     @Test
     @WithMockUser(roles = "USER")
@@ -40,21 +27,30 @@ public class HomeControllerTests {
     }
 
     @Test
-    @WithMockUser
+    @WithMockUser(roles = "ADMIN")
     public void 권한검사() throws Exception {
         //Given
-        String permission = "isAdmin";
-        HashSet<GrantedAuthority> authorities = new HashSet<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
 
         //When
-        doReturn(authorities).when(authentication).getAuthorities();
-        System.out.println("권한검사 결과 : " + customPermissionEvaluator.hasPermission(authentication, null, permission));
 
         //Then
         mockMvc.perform(get("/test"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("test"))
+                .andDo(print());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void 에러() throws Exception {
+        // Given
+
+        // When
+
+        // Then
+        mockMvc.perform(get("/admin"))
+                .andExpect(status().isForbidden())
+                .andExpect(view().name("error403"))
                 .andDo(print());
     }
 }
